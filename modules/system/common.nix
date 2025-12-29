89b00cefd61eb2232159b619cfab5fa10f51c7e6
Eran Knafo <eranknafo2001@gmail.com>
2025-12-29 18:22:50 +0200

    Add Hyprland and nix-community binary caches

--- modules/system/common.nix
+++ modules/system/common.nix
@@ -22,7 +22,17 @@
   };
 
   networking.networkmanager.enable = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
 
